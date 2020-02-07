8912f1fba232043ef2eef1c11b3c76114562a50a
Ubuntu <ubuntu@vamshi-cloudsuite-client.cloud.compas.cs.stonybrook.edu>
2020-02-07 22:42:53 +0000

    made the run calls to be terminated in case of some exception and updated doc.

--- benchmarks/django-workload/uwsgi/run_uwsgi.sh
+++ benchmarks/django-workload/uwsgi/run_uwsgi.sh
@@ -3,6 +3,8 @@
 # This function will wait until the specified port on the specified machine is available
 # Parameters: $1 = the IP; $2 = the port; $3 = the name of the service
 
+set -e
+
 wait_port() {
   while ! netcat -w 5 "$1" "$2"; do
     echo "Waiting for $3 ..."
